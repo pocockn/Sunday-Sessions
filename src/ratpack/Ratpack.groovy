04592e0f469c8e6bfc78b358bf35f84c4868540e
Nick Pocock <nick.pocock@informa.com>
2016-07-05 09:21:09 +0100

    Successfully grabbing the query params in Ratpack

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,6 +1,7 @@
 import facebook.FaceBookLogin
 import ratpack.groovy.template.MarkupTemplateModule
 import ratpack.handlebars.HandlebarsModule
+import ratpack.path.PathTokens
 
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.handlebars.Template.handlebarsTemplate
@@ -17,8 +18,11 @@ ratpack {
             String loginDialogUrlString = faceBookLogin.buildLoginDialogUrlString()
             render handlebarsTemplate("index.html", model: loginDialogUrlString)
         }
-        get('success/:code?') {
-            render handlebarsTemplate("success.html")
+        get('success') {
+            // Grab our access token returned by Facebook
+            def code = request.queryParams.code
+            println code
+            render handlebarsTemplate("success.html", model: code)
         }
 
         files { dir "public" }
