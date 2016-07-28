0fab6376a50be87d86e0d6b0f8dc755bed05a502
Nick Pocock <nick.pocock@informa.com>
2016-07-28 12:57:11 +0100

    refactored location to use ratpack promise

--- src/main/groovy/handlers/GrabUsersLocation.groovy
+++ src/main/groovy/handlers/GrabUsersLocation.groovy
@@ -2,6 +2,7 @@ package handlers
 
 import geolite.GeoLiteGetCity
 import groovy.util.logging.Slf4j
+import ratpack.exec.Promise
 import ratpack.handling.Context
 import ratpack.handling.InjectionHandler
 
@@ -14,17 +15,20 @@ import static ratpack.handlebars.Template.handlebarsTemplate
 class GrabUsersLocation extends InjectionHandler {
 
     void handle(Context ctx, GeoLiteGetCity geoLiteGetCity) throws Exception {
-        LinkedHashMap<String, String> coordinates = getUsersLocation(geoLiteGetCity)
-        ctx.render handlebarsTemplate("locationTest.html", model: coordinates)
+        getUsersLocation(geoLiteGetCity).then { coordinates ->
+            log.info("${coordinates}")
+            ctx.render handlebarsTemplate("locationTest.html", model: coordinates)
+        }
+
     }
 
-    private LinkedHashMap<String, String> getUsersLocation(GeoLiteGetCity geoLiteGetCity) {
+    private Promise<LinkedHashMap<String, String>> getUsersLocation(GeoLiteGetCity geoLiteGetCity) {
         def location = geoLiteGetCity.getLocationFromIP(geoLiteGetCity.findIPAddress())
         Map<String, String> coordinates = [:]
         coordinates.put("longitude", location.location.longitude.toString())
         coordinates.put("latitude", location.location.latitude.toString())
         log.info("Latitude ${location.location.latitude}")
         log.info("Longitude ${location.location.longitude}")
-        coordinates
+        Promise.value(coordinates)
     }
 }
