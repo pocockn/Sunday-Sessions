bde27c3105ea944b8384414e8956547201299145
Nick Pocock <nick.pocock@informa.com>
2016-07-29 15:45:26 +0100

    added promises support for fb login

--- src/main/groovy/handlers/FBLoginSuccessHandler.groovy
+++ src/main/groovy/handlers/FBLoginSuccessHandler.groovy
@@ -3,6 +3,7 @@ package handlers
 import facebook.FaceBookLogin
 import facebook.GraphReaderCalls
 import groovy.util.logging.Slf4j
+import ratpack.exec.Promise
 import ratpack.handling.Context
 import ratpack.handling.InjectionHandler
 
@@ -16,18 +17,25 @@ class FBLoginSuccessHandler extends InjectionHandler {
 
     public void handle(Context ctx, GraphReaderCalls graphReaderCalls) throws Exception {
         def code = ctx.request.queryParams.code
-        String accessToken = obtainAccessCode(code)
-        graphReaderCalls.setUpConnection(accessToken)
-        def image = graphReaderCalls.getProfilePicture()
-        ctx.render handlebarsTemplate("success.html", model: image)
+        if (code) {
+            obtainAccessCode(code).then { accessToken ->
+                graphReaderCalls.setUpConnection(accessToken)
+                def image = graphReaderCalls.getProfilePicture()
+                ctx.render handlebarsTemplate("success.html", model: image)
+            }
+        } else {
+            ctx.render handlebarsTemplate("error.html")
+        }
+
     }
 
-    protected String obtainAccessCode(String code) {
+    protected Promise<String> obtainAccessCode(String code) {
         FaceBookLogin faceBookLogin = new FaceBookLogin()
         String accessToken = faceBookLogin.exchangeCodeForAccessToken(code).accessToken
-        return accessToken
+        Promise.value(accessToken)
     }
 }
+
 
 
 
--- src/test/groovy/handlers/MatchUserHandlerTest.groovy
+++ src/test/groovy/handlers/MatchUserHandlerTest.groovy
@@ -14,9 +14,9 @@ class MatchUserHandlerTest extends Specification {
     def "Test radius function brings back correct value"() {
         given:
         // America
-        User userAmerica = new User('124', 40.7128, 74.0059)
+        User userAmerica = new User('124', 'Nick Pocock', 40.7128, 74.0059)
         // Canada
-        User userCanada = new User('125', 49.2827, 123.1207)
+        User userCanada = new User('125', 'Nick Pocock', 49.2827, 123.1207)
 
         when:
         def distance = CompareLatLongRadius.distance(userAmerica.latitude, userAmerica.longitude, userCanada.latitude, userCanada.longitude)
@@ -28,14 +28,14 @@ class MatchUserHandlerTest extends Specification {
     def "Given a list of users, find users within 20 mile radius"() {
         given:
         List<User> users = []
-        User userNewYork = new User('124', 40.7128, 74.0059)
-        User userNewJersey = new User('124', 40.0583, 74.4057)
-        User userCanada = new User('125', 49.2827, 123.1207)
+        User userNewYork = new User('124', 'Nick Pocock', 40.7128, 74.0059)
+        User userNewJersey = new User('124', 'Nick Pocock', 40.0583, 74.4057)
+        User userCanada = new User('125', 'Nick Pocock', 49.2827, 123.1207)
         users.add(userNewYork)
         users.add(userNewJersey)
         users.add(userCanada)
 
-        User userAmericaTwo = new User('124', 40.8128, 74.3)
+        User userAmericaTwo = new User('124', 'Nick Pocock', 40.8128, 74.3)
 
         when:
         def usersSize = CompareLatLongRadius.CoordDistanceList(userAmericaTwo.latitude, userAmericaTwo.longitude, users)
